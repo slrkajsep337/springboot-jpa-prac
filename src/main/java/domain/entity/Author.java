7cb37a177716bb60f315a2b927a83e5ca21e8ab6
이수진 <slrkajsep337@naver.com>
2022-11-23 13:11:23 +0900

    Author entity 생성

---
+++ src/main/java/domain/entity/Author.java
@@ -0,0 +1,23 @@
+package domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+    @Id
+    private Long id;
+    private String name;
+
+
+
+}
+
+
