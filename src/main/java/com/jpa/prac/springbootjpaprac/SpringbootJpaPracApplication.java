963231b4f4abc9bdc192079173c03202e7b36a1f
이수진 <slrkajsep337@naver.com>
2022-11-23 11:44:16 +0900

    Initial commit

---
+++ src/main/java/com/jpa/prac/springbootjpaprac/SpringbootJpaPracApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.prac.springbootjpaprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpa/prac/springbootjpaprac/SpringbootJpaPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.prac.springbootjpaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
