c3d0b4cb91bb7bd760223cb3455082040b173e2a
이수진 <slrkajsep337@naver.com>
2022-11-25 01:48:37 +0900

    entity 생성

--- src/main/java/com/jpa/prac/springbootjpaprac/domain/entity/Hospital.java
+++ src/main/java/com/jpa/prac/springbootjpaprac/domain/entity/Hospital.java
@@ -1,16 +1,26 @@
 package com.jpa.prac.springbootjpaprac.domain.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Hospital {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String hospitalName;
     private String roadNameAddress;
+
+    //양방향매핑 병원:one 리뷰:many  //lazy: 지연로딩 -> 권장
+    @OneToMany(mappedBy = "hospital", fetch = FetchType.LAZY)
+    private List<Review> reviews;
+
 }
--- src/main/java/com/jpa/prac/springbootjpaprac/domain/entity/Review.java
+++ src/main/java/com/jpa/prac/springbootjpaprac/domain/entity/Review.java
@@ -1,17 +1,30 @@
 package com.jpa.prac.springbootjpaprac.domain.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private int hospitalId;
+    private Long id;
+
     private String title;
     private String content;
-    private String patientName;
+    private String userName;
+
+
+    //양방향 매핑, 병원과 매핑해주기
+    @ManyToOne  //review 입장에서
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+
 }
