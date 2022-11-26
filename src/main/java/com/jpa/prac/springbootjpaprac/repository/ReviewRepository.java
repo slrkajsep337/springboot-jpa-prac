fc7eeff7b5f78b7e6ea4847f41c57ed13a8da1f6
이수진 <slrkajsep337@naver.com>
2022-11-26 16:37:25 +0900

    reviewRepository

--- src/main/java/com/jpa/prac/springbootjpaprac/repository/ReviewRepository.java
+++ src/main/java/com/jpa/prac/springbootjpaprac/repository/ReviewRepository.java
@@ -1,7 +1,12 @@
 package com.jpa.prac.springbootjpaprac.repository;
 
+import com.jpa.prac.springbootjpaprac.domain.entity.Hospital;
 import com.jpa.prac.springbootjpaprac.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByHospital(Hospital hospital);
 }
