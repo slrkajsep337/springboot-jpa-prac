77c1e0bdfed99f46a847203fa4ab46d51adfbffe
이수진 <slrkajsep337@naver.com>
2022-11-27 21:16:03 +0900

    해당 병원의 정보+ 전체리뷰 조회하기 기능 구현

--- src/main/java/com/jpa/prac/springbootjpaprac/controller/HospitalController.java
+++ src/main/java/com/jpa/prac/springbootjpaprac/controller/HospitalController.java
@@ -1,9 +1,11 @@
 package com.jpa.prac.springbootjpaprac.controller;
 
 
+import com.jpa.prac.springbootjpaprac.domain.dto.HospitalReadResponse;
 import com.jpa.prac.springbootjpaprac.domain.dto.ReviewCreateRequest;
 import com.jpa.prac.springbootjpaprac.domain.dto.ReviewCreateResponse;
 import com.jpa.prac.springbootjpaprac.domain.dto.ReviewReadResponse;
+import com.jpa.prac.springbootjpaprac.domain.entity.Hospital;
 import com.jpa.prac.springbootjpaprac.domain.entity.Review;
 import com.jpa.prac.springbootjpaprac.repository.ReviewRepository;
 import com.jpa.prac.springbootjpaprac.service.HospitalService;
@@ -21,27 +23,29 @@ import java.util.Optional;
 @Slf4j
 public class HospitalController {
 
-    private final ReviewRepository rr;
     private final ReviewService rs;
     private final HospitalService hs;
 
-    public HospitalController(ReviewRepository rr, ReviewService rs, HospitalService hs) {
-        this.rr = rr;
+    public HospitalController(ReviewService rs, HospitalService hs) {
         this.rs = rs;
         this.hs = hs;
     }
 
-    //리뷰 1개 입력받는 기능
+    //리뷰 1개 등록하는 기능 -> reviewCreateRequest, reviewCreateResponse 사용
     @PostMapping("/{id}/reviews")
+    //@RequestBody : 여러 정보를 requestBody에 jason 형식으로 입력한다
     public ResponseEntity<ReviewCreateResponse> add(@RequestBody ReviewCreateRequest requestDto){
         log.info("{}",requestDto);
         return ResponseEntity.ok().body(rs.createReview(requestDto));
 
     }
 
+
+    //조회 하는 기능들 3가지 -> reviewReadResponse 사용
+
     //리뷰 1개 조회 기능
     //GET /api/v1/reviews/{id}
-    @GetMapping("/{id}")
+    @GetMapping("review/{id}")
     public ResponseEntity<ReviewReadResponse> getOneReview(@PathVariable Long id) {
         Review review = rs.getReview(id);
         ReviewReadResponse response = ReviewReadResponse.builder()
@@ -55,20 +59,19 @@ public class HospitalController {
 
     }
 
-    //해당 병원의 전체 리뷰 조회하기
-    @GetMapping("/{hospitalId}/reviews")
+    //해당 병원의 전체 리뷰 조회하기 (병원정보는 X)
+    @GetMapping("reviews/{hospitalId}")
     public ResponseEntity<List<ReviewReadResponse>> getReviews(@PathVariable Long hospitalId) {
         return ResponseEntity.ok().body(rs.findAllByHospitalId(hospitalId));
     }
 
 
-    //해당 병원의 리뷰 + 병원 정보까지 조회하기
-//    @GetMapping("/{id}")
-//    public ResponseEntity<ReviewReadResponse> getReviewAndHospital(@PathVariable Long id) {
-//        Review review = rs.getReview(id);
-//        ReviewReadResponse response = ReviewReadResponse.toDto(review);
-//        return ResponseEntity.ok().body(response);
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<HospitalReadResponse> getHospital(@PathVariable Long id) {
+        Hospital hospital = hs.findById(id);
+        HospitalReadResponse response = HospitalReadResponse.toDto(hospital);
+        return ResponseEntity.ok().body(response);
+    }
 
 
 
